81cb3dbef542d1ff5d7da46ceef3e05dff2402d9
kevin <kevin0x00@163.com>
2023-09-06 13:16:54 +0800

    clean

--- pargen/include/lr/lr.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEVCC_PARGEN_INCLUDE_LR_LR_H
-#define KEVCC_PARGEN_INCLUDE_LR_LR_H
-
-#include "pargen/include/lr/collection.h"
-#include "pargen/include/lr/table.h"
-#include "pargen/include/lr/print.h"
-
-#endif
