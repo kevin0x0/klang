b1f58a0ae2ae13b2817d56a6a8c5cf608bc96647
kevin <kevin0x00@163.com>
2023-09-18 15:14:53 +0800

    add kev_str_is_prefix()

--- utils/include/string/kev_string.h
+++ utils/include/string/kev_string.h
@@ -8,12 +8,17 @@ char* kev_str_concat(const char* str1, const char* str2);
 char* kev_str_copy_len(const char* str, size_t len);
 char* kev_str_concat_len(const char* str1, const char* str2, size_t len1, size_t len2);
 char* kev_trunc_leaf(const char* path);
+static inline size_t kev_str_is_prefix(const char* prefix, const char* str);
 static inline size_t kev_str_prefix(const char* prefix, const char* str);
 
 static inline size_t kev_str_prefix(const char* prefix, const char* str) {
   size_t i = 0;
   while (prefix[i] == str[i] && prefix[i] != '\0') ++i;
   return i;
+}
+
+static inline size_t kev_str_is_prefix(const char* prefix, const char* str) {
+  return prefix[kev_str_prefix(prefix, str)] == '\0';
 }
 
 #endif
