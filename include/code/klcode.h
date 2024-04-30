5a2fbaa4594994ecfb4ef2de68ceb55e21638f87
Ji Kaiwen <kevin0x00@163.com>
2024-04-30 15:02:13 +0800

    save

--- include/code/klcode.h
+++ include/code/klcode.h
@@ -21,23 +21,23 @@ typedef struct tagKlCodeGenConfig {
 
 /* this is the same structure as KlRefInfo used in compiler */
 typedef struct tagKlCRefInfo {
+  unsigned index;
   bool on_stack;
-  uint8_t index;
 } KlCRefInfo;
 
 struct tagKlCode {
   KlCRefInfo* refinfo;
-  size_t nref;
   KlConstant* constants;
-  size_t nconst;
   KlInstruction* code;
-  size_t codelen;
   KlFilePosition* posinfo;
   KlCode** nestedfunc;        /* functions created inside this function */
-  size_t nnested;
   KlStrTbl* strtbl;
-  size_t nparam;              /* number of parameters */
-  size_t framesize;           /* stack frame size of this klang function */
+  unsigned nref;
+  unsigned nconst;
+  unsigned codelen;
+  unsigned nnested;
+  unsigned nparam;            /* number of parameters */
+  unsigned framesize;         /* stack frame size of this klang function */
 };
 
 
@@ -50,7 +50,7 @@ void klcode_delete(KlCode* code);
 KlCode* klcode_create_fromcst(KlCst* cst, KlStrTbl* strtbl, KlCodeGenConfig* config);
 KlCode* klcode_create_fromfile(Ki* file);
 
-void klcode_dump(KlCode* code, Ko* file);
+bool klcode_dump(KlCode* code, Ko* file);
 void klcode_print(KlCode* code, Ko* out);
 
 #endif
--- src/code/klcode_dump.c
+++ src/code/klcode_dump.c
@@ -1,4 +1,5 @@
 #include "include/code/klcode.h"
+#include <setjmp.h>
 
 struct a {
   KlCRefInfo* refinfo;
@@ -15,21 +16,70 @@ struct a {
   size_t framesize;           /* stack frame size of this klang function */
 };
 
-#define KLCODEDUMP_MAGIC_SZ         (4)
+#define KLCODEDUMP_MAGIC_SZ         (8)
+#define KLCODEDUMP_MAGIC            ("\aklang")
 #define KLCODEDUMP_BIG_ENDIAN       (0)
 #define KLCODEDUMP_SMALL_ENDIAN     (1)
 #define KLCODEDUMP_VERSION          (0)
 
 typedef unsigned char uchar;
 
-typedef struct tafKlCodeDumpHeader {
+typedef struct tagKlCodeDumpHeader {
   char magic[KLCODEDUMP_MAGIC_SZ];
   uchar endian;
   uchar version;
-
+  uchar unsignedsize;           /* sizeof (unsigned) */
+  uchar intsize;                /* sizeof (KlCInt) */
+  uchar floatsize;              /* sizeof (KlCFloat) */
+  uchar reserved[32 - (KLCODEDUMP_MAGIC_SZ + 5)];
 } KlCodeDumpHeader;
 
-void klcode_dump(KlCode* code, Ko* file) {
+typedef struct tagKlCodeHeader {
+  unsigned nref;
+  unsigned nconst;
+  unsigned codelen;
+  unsigned nnested;
+  unsigned nparam;
+  unsigned framesize;
+  uchar hasposinfo;
+  uchar reserved[4];
+} KlCodeHeader;
+
+typedef struct tagKlCodeDumpState {
+  Ko* file;
+  jmp_buf jmppos;
+} KlCodeDumpState;
+
+kl_noreturn static void klcode_dump_failure(KlCodeDumpState* state);
+kl_noreturn static void klcode_dump_code(KlCodeDumpState* state, KlCode* code);
+static void klcode_dump_codeheader(KlCodeDumpState* state, KlCodeHeader* header);
+
+
+bool klcode_dump(KlCode* code, Ko* file) {
 }
 
 KlCode* klcode_create_fromfile(Ki* file);
+
+
+static void klcode_dump_codeheader(KlCodeDumpState* state, KlCodeHeader* header) {
+  if (kl_unlikely(sizeof (KlCodeHeader) != ko_write(state->file, header, sizeof (KlCodeHeader))))
+    klcode_dump_failure(state);
+}
+
+kl_noreturn static void klcode_dump_code(KlCodeDumpState* state, KlCode* code) {
+  KlCodeHeader header = {
+    .nref = code->nref,
+    .nconst = code->nconst,
+    .nparam = code->nparam,
+    .nnested = code->nnested,
+    .codelen = code->codelen,
+    .framesize = code->framesize,
+    .hasposinfo = code->posinfo != NULL,
+  };
+  memset(header.reserved, 0, sizeof (header.reserved));
+  klcode_dump_codeheader(state, &header);
+}
+
+kl_noreturn static void klcode_dump_failure(KlCodeDumpState* state) {
+  longjmp(state->jmppos, 1);
+}
