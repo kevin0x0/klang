b4b8c09bb9a83a192e99c25a48772a7c8c21c8da
Ji Kaiwen <kevin0x00@163.com>
2024-05-13 00:03:59 +0800

    fix bug

--- src/api/klapi_call.c
+++ src/api/klapi_call.c
@@ -1,4 +1,5 @@
 #include "include/klapi.h"
+#include "include/misc/klutils.h"
 #include "include/value/klstate.h"
 #include "deps/k/include/lib/lib.h"
 
@@ -6,11 +7,15 @@ KlException klapi_trycall(KlState* state, KlValue* callable, size_t narg, size_t
   ptrdiff_t errhandler_save = klexec_savestack(state, klstate_stktop(state) - narg - 1);
   ptrdiff_t respos_save = klexec_savestack(state, respos);
   kl_assert(errhandler_save >= 0, "errhandler not provided");
+  KlCallInfo* currci = klstate_currci(state);
   KlException exception = klapi_call(state, callable, narg, nret, respos);
   /* if no exception occurred, just return */
   if (kl_likely(!exception)) return exception;
   /* else handle exception */
-  return klapi_call(state, klexec_restorestack(state, errhandler_save), 0, nret, klexec_restorestack(state, respos_save));
+  exception = klapi_call(state, klexec_restorestack(state, errhandler_save), 0, nret, klexec_restorestack(state, respos_save));
+  if (kl_unlikely(exception)) return exception;
+  klstate_setcurrci(state, currci);
+  return KL_E_NONE;
 }
 
 KlException klapi_loadlib(KlState* state, const char* libpath, const char* entryfunction) {
@@ -28,6 +33,7 @@ KlException klapi_loadlib(KlState* state, const char* libpath, const char* entry
   }
 
   /* call the entry function */
-  klapi_pushcfunc(state, init);
+  KlException exception = klapi_pushcfunc(state, init);
+  if (kl_unlikely(exception)) return exception;
   return klapi_call(state, klapi_access(state, -1), 0, KLAPI_VARIABLE_RESULTS, klapi_access(state, -1));
 }
