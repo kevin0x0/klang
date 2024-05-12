027c968448852de354167108e8a134e5ece502bf
Ji Kaiwen <kevin0x00@163.com>
2024-05-13 00:06:11 +0800

    implement two basic library

---
+++ src/langlib/rtcpl/kllib_rtcpl.c
@@ -0,0 +1,286 @@
+#include "include/klapi.h"
+#include "include/code/klcode.h"
+#include "include/parse/klparser.h"
+
+static KlException kllib_codetokfunc(KlState* state, KlCode* code);
+static KlException kllib_mkclosure(KlState* state, KlCode* code);
+static void kllib_setconstant_nonstring(KlValue* val, KlConstant* constant);
+/* compiler */
+static KlException kllib_rtcpl_compiler(KlState* state);
+/* byte code loader */
+static KlException kllib_rtcpl_bcloader(KlState* state);
+
+KlException kllib_init(KlState* state);
+
+
+KlException kllib_init(KlState* state) {
+  KlException exception = klapi_allocstack(state, 2);
+  if (kl_unlikely(exception)) return exception;
+  klapi_setcfunc(state, -2, kllib_rtcpl_compiler);
+  klapi_setcfunc(state, -1, kllib_rtcpl_bcloader);
+  return klapi_return(state, 2);
+}
+
+static KlException kllib_rtcpl_compiler(KlState* state) {
+  kl_assert(klapi_narg(state) == 4, "expected exactly 4 argmuments");
+  kl_assert(klapi_checktypeb(state, 0, KL_USERDATA) &&
+            klapi_checktypeb(state, 1, KL_USERDATA) &&
+            klapi_checktypeb(state, 2, KL_STRING) &&
+            klapi_checktypeb(state, 3, KL_STRING) || klapi_checkypeb(state, 3, KL_NIL),
+            "expected Ki, Ko(use type tag: KL_USERDATA), KL_STRING, KL_STRING(or nil)");
+
+  Ki* input = klcast(Ki*, klapi_getuserdatab(state, 0));
+  Ko* err = klcast(Ko*, klapi_getuserdatab(state, 1));
+  KlString* inputname = klapi_getstringb(state, 2);
+  KlString* srcfile = klapi_checktypeb(state, 3, KL_STRING) ? klapi_getstringb(state, 3) : NULL;
+
+
+  KlError klerr;
+  klerror_init(&klerr, err);
+  KlStrTbl* strtbl = klstrtbl_create();
+  if (kl_unlikely(!strtbl))
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while initializing compiler");
+  KlLex lex;
+  if (kl_unlikely(!kllex_init(&lex, input, &klerr, klstring_content(inputname), strtbl))) {
+    klstrtbl_delete(strtbl);
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while initializing compiler");
+  }
+  KlParser parser;
+  if (kl_unlikely(!klparser_init(&parser, strtbl, klstring_content(inputname), &klerr))) {
+    kllex_destroy(&lex);
+    klstrtbl_delete(strtbl);
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while initializing compiler");
+  }
+
+  /* parse */
+  kllex_next(&lex);
+  KlAstStmtList* ast = klparser_file(&parser, &lex);
+  kllex_destroy(&lex);
+  if (klerror_nerror(&klerr) != 0) {
+    if (ast) klast_delete(ast);
+    klstrtbl_delete(strtbl);
+    /* syntax error, not an exception, return nothing */
+    return klapi_return(state, 0);
+  }
+
+  KlStrDesc srcfilename;
+  if (!srcfile) {
+    srcfilename = (KlStrDesc) { .id = 0, .length = 0 };
+  } else {
+    const char* str = klstrtbl_newstring(strtbl, klstring_content(srcfile));
+    if (kl_unlikely(!str)) {
+      klast_delete(ast);
+      klstrtbl_delete(strtbl);
+      return klapi_throw_internal(state, KL_E_OOM, "out of memory while initializing code generator");
+    }
+    srcfilename = (KlStrDesc) { .id = klstrtbl_stringid(strtbl, str), .length = klstring_length(srcfile) };
+  }
+  /* genenrate code */
+  KlCodeGenConfig config = {
+    .inputname = klstring_content(inputname),
+    .srcfile = srcfilename,
+    .input = input,
+    .klerr = &klerr,
+    .debug = false,
+    .posinfo = true,
+  };
+  KlCode* code = klcode_create_fromast(ast, strtbl, &config);
+  klast_delete(ast);
+  if (klerror_nerror(&klerr) != 0) {
+    if (code) klcode_delete(code);
+    klstrtbl_delete(strtbl);
+    /* semantic error, not an exception, return nothing */
+    return klapi_return(state, 0);
+  }
+  KlException exception = kllib_mkclosure(state, code);
+  klcode_delete(code);
+  klstrtbl_delete(strtbl);
+  if (kl_unlikely(exception)) return exception;
+  kl_assert(klapi_framesize(state) == klapi_narg(state) + 1, "");
+  return klapi_return(state, 1);
+}
+
+static KlException kllib_rtcpl_bcloader(KlState* state) {
+  kl_assert(klapi_narg(state) == 4, "expected exactly 4 argmuments");
+  kl_assert(klapi_checktypeb(state, 0, KL_USERDATA) && klapi_checktypeb(state, 1, KL_USERDATA),
+            "expected Ki, Ko(use type tag: KL_USERDATA), KL_STRING, KL_STRING(or nil)");
+
+  Ki* input = klcast(Ki*, klapi_getuserdatab(state, 0));
+  Ko* err = klcast(Ko*, klapi_getuserdatab(state, 1));
+
+  KlError klerr;
+  klerror_init(&klerr, err);
+  KlStrTbl* strtbl = klstrtbl_create();
+  if (kl_unlikely(!strtbl))
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while initializing bytecode loader");
+
+  KlUnDumpError error;
+  KlCode* code = klcode_undump(input, strtbl, &error);
+  if (kl_unlikely(!code)) {
+    const char* errmsg = NULL;
+    switch (error) {
+      case KLUNDUMP_ERROR_BAD: {
+        errmsg = "bad binary file";
+        break;
+      }
+      case KLUNDUMP_ERROR_SIZE: {
+        errmsg = "type size does not match";
+        break;
+      }
+      case KLUNDUMP_ERROR_OOM: {
+        errmsg = "out of memory";
+        break;
+      }
+      case KLUNDUMP_ERROR_MAGIC: {
+        errmsg = "not a bytecode file";
+        break;
+      }
+      case KLUNDUMP_ERROR_ENDIAN: {
+        errmsg = "byte order does not match";
+        break;
+      }
+      case KLUNDUMP_ERROR_VERSION: {
+        errmsg = "version does not match";
+        break;
+      }
+      default: {
+        errmsg = "unknown error";
+        break;
+      }
+    }
+    klstrtbl_delete(strtbl);
+    ko_printf(err, "undump failure: %s\n", errmsg);
+    return klapi_return(state, 0);
+  }
+  KlException exception = kllib_mkclosure(state, code);
+  klcode_delete(code);
+  klstrtbl_delete(strtbl);
+  if (kl_unlikely(exception)) return exception;
+  kl_assert(klapi_framesize(state) == klapi_narg(state) + 1, "");
+  return klapi_return(state, 1);
+}
+
+static KlException kllib_mkclosure(KlState* state, KlCode* code) {
+  KlException exception = kllib_codetokfunc(state, code);
+  if (kl_unlikely(exception)) return exception;
+  KlKClosure* kclo = klkclosure_create(klstate_getmm(state), klapi_getobj(state, -1, KlKFunction*), NULL, klstate_reflist(state), NULL);
+  if (kl_unlikely(!kclo))
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while creating klang closure");
+  klapi_setobj(state, -1, kclo, KL_KCLOSURE);
+  return KL_E_NONE;
+}
+
+static KlException kllib_codetokfunc(KlState* state, KlCode* code) {
+  size_t nnested = code->nnested;
+  KlCode** nestedfunc = code->nestedfunc;
+  for (size_t i = 0; i < nnested; ++i) {
+    KlException exception = kllib_codetokfunc(state, nestedfunc[i]);
+    if (kl_unlikely(exception)) return exception;
+  }
+  KlMM* klmm = klstate_getmm(state);
+  KlInstruction* insts = (KlInstruction*)klmm_alloc(klmm, code->codelen * sizeof (KlInstruction));
+  if (kl_unlikely(!insts))
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while creating klang function");
+  memcpy(insts, code->code, code->codelen * sizeof (KlInstruction));
+  KlKFunction* kfunc = klkfunc_alloc(klmm, insts, code->codelen, code->nconst, code->nref, code->nnested, code->framesize, code->nparam);
+  if (kl_unlikely(!kfunc)) {
+    klmm_free(klmm, insts, code->codelen * sizeof (KlInstruction));
+    return klapi_throw_internal(state, KL_E_OOM, "out of memory while creating klang function");
+  }
+
+  size_t nstkused = 0;  /* stack used from now */
+  /* init source file info and posinfo */
+  if (code->srcfile.length != 0) {
+    KlException exception = klapi_pushstring_buf(state, klstrtbl_getstring(code->strtbl, code->srcfile.id), code->srcfile.length);
+    if (kl_unlikely(exception)) {
+      klkfunc_initabort(klmm, kfunc);
+      klmm_free(klmm, insts, code->codelen * sizeof (KlInstruction));
+      return exception;
+    }
+    ++nstkused;
+    klkfunc_setsrcfile(kfunc, klapi_getstring(state, -1));
+  }
+  if (code->posinfo != NULL) {
+    KlKFuncFilePosition* posinfo = (KlKFuncFilePosition*)klmm_alloc(klmm, code->codelen * sizeof (KlKFuncFilePosition));
+    if (kl_unlikely(!posinfo)) {
+      klkfunc_initabort(klmm, kfunc);
+      klmm_free(klmm, insts, code->codelen * sizeof (KlInstruction));
+      return klapi_throw_internal(state, KL_E_OOM, "out of memory while creating klang function");
+    }
+    for (size_t i = 0; i < code->codelen; ++i) {
+      posinfo[i].begin = code->posinfo[i].begin;
+      posinfo[i].end = code->posinfo[i].end;
+    }
+  }
+  /* init constants */
+  size_t nconst = code->nconst;
+  KlConstant* concode = code->constants;
+  KlValue* conkfunc = klkfunc_constants(kfunc);
+  for (size_t i = 0; i < nconst; ++i) {
+    if (concode[i].type != KLC_STRING) { /* not a gc constant */
+      kllib_setconstant_nonstring(&conkfunc[i], &concode[i]);
+    } else {  /* a gc constant, push it to stack to avoid being collected */
+      KlException exception = klapi_pushstring_buf(state, klstrtbl_getstring(code->strtbl, concode[i].string.id), concode[i].string.length);
+      if (kl_unlikely(exception)) {
+        klkfunc_initabort(klmm, kfunc);
+        klmm_free(klmm, insts, code->codelen * sizeof (KlInstruction));
+        return exception;
+      }
+      ++nstkused;
+      klvalue_setvalue(&conkfunc[i], klapi_access(state, -1));
+    }
+  }
+  /* init refinfo */
+  size_t nref = code->nref;
+  KlCRefInfo* refcode = code->refinfo;
+  KlRefInfo* refkfunc = klkfunc_refinfo(kfunc);
+  for (size_t i = 0; i < nref; ++i) {
+    refkfunc[i].index = refcode[i].index;
+    refkfunc[i].on_stack = refcode[i].on_stack;
+  }
+  if (nnested == 0) {
+    if (nstkused == 0) {
+      klapi_allocstack(state, 1);
+    } else {
+      klapi_pop(state, nnested - 1);
+    }
+    klapi_setobj(state, -1, kfunc, KL_KFUNCTION);
+    klkfunc_initdone(klmm, kfunc);
+    return KL_E_NONE;
+  }
+  KlValue* subfuncs_onstk = klapi_access(state, -(nstkused + code->nnested));
+  KlKFunction** subfuncs = klkfunc_subfunc(kfunc);
+  for (size_t i = 0; i < nnested; ++i) {
+    kl_assert(klvalue_checktype(&subfuncs_onstk[i], KL_KFUNCTION), "");
+    subfuncs[i] = klvalue_getobj(&subfuncs_onstk[i], KlKFunction*);
+  }
+  klapi_pop(state, (nstkused + nnested) - 1);
+  klapi_setobj(state, -1, kfunc, KL_KFUNCTION);
+  klkfunc_initdone(klmm, kfunc);
+  return KL_E_NONE;
+}
+
+static void kllib_setconstant_nonstring(KlValue* val, KlConstant* constant) {
+  switch (constant->type) {
+    case KLC_INT: {
+      klvalue_setint(val, constant->intval);
+      return;
+    }
+    case KLC_BOOL: {
+      klvalue_setbool(val, constant->boolval == KLC_TRUE ? KL_TRUE : KL_FALSE);
+      return;
+    }
+    case KLC_NIL: {
+      klvalue_setnil(val);
+      return;
+    }
+    case KLC_FLOAT: {
+      klvalue_setfloat(val, constant->floatval);
+      return;
+    }
+    default: {
+      kl_assert(false, "unreachable");
+      return;
+    }
+  }
+}
---
+++ src/langlib/traceback/kllib_tb.c
@@ -0,0 +1,222 @@
+#include "include/klapi.h"
+
+#include <stdio.h>
+
+typedef struct tagKlFmtConfig {
+  unsigned tabstop;
+  unsigned maxtextline;
+  char curl;
+  char zerocurl;
+  char* promptmsg;
+  char* prompttext;
+  char* promptnorm;
+} KlFmtConfig;
+
+static unsigned kllib_tb_helper_locateline(FILE* input, long offset);
+static bool kllib_tb_helper_showline_withcurl(KlFmtConfig* fmt, FILE* err, FILE* input, long begin, long end);
+
+static void kllib_tb_printCframe(KlFmtConfig* fmt, FILE* err);
+static void kllib_tb_printKframe_noany(KlFmtConfig* fmt, FILE* err);
+static void kllib_tb_printKframe_noposinfo(KlFmtConfig* fmt, FILE* err, const char* inputname);
+static void kllib_tb_printKframe_nosrcfile(KlFmtConfig* fmt, FILE* err, long begin, long end);
+static void kllib_tb_printKframe_openfilefailure(KlFmtConfig* fmt, FILE* err, const char* inputname, long begin, long end);
+static void kllib_tb_printKframe(KlFmtConfig* fmt, FILE* err, FILE* input, const char* inputname, long begin, long end);
+
+static void kllib_tb_printframe(KlFmtConfig* fmt, FILE* err, KlCallInfo* callinfo);
+static void kllib_tb_dotraceback(KlFmtConfig* fmt, FILE* err, KlCallInfo* from, KlCallInfo* end);
+static KlException kllib_tb_main(KlState* state);
+
+KlException kllib_init(KlState* state) {
+  KlException exception = klapi_pushcfunc(state, kllib_tb_main);
+  if (kl_unlikely(exception)) return exception;
+  return klapi_return(state, 1);
+}
+
+static KlException kllib_tb_main(KlState* state) {
+  KlCallInfo* currci = klstate_currci(state);
+  kl_assert(currci->status & KLSTATE_CI_STATUS_CFUN && currci->cfunc == kllib_tb_main, "");
+  FILE* err = NULL;
+  if (klapi_narg(state) == 0) {
+    err = stderr;
+  } else {
+    KlValue* firstarg = klapi_accessb(state, 0);
+    kl_assert(klvalue_checktype(firstarg, KL_USERDATA),  "expected file pointer(use type tag: KL_USERDATA)");
+    err = klcast(FILE*, klvalue_getuserdata(firstarg));
+  }
+  KlFmtConfig fmtconfig = {
+    .curl = '~',
+    .zerocurl = '^',
+    .tabstop = 8,
+    .maxtextline = 3,
+    .promptnorm = "|| ",
+    .prompttext = "||== ",
+    .promptmsg = "|| ",
+  };
+  /* the actual most recent callinfo is this function itself, so use currci->prev
+   * instead of currci. */
+  kllib_tb_dotraceback(&fmtconfig, err, currci->prev, klstate_baseci(state));
+  return klapi_return(state, 0);
+}
+
+static void kllib_tb_dotraceback(KlFmtConfig* fmt, FILE* err, KlCallInfo* from, KlCallInfo* end) {
+  while (from != end) {
+    kllib_tb_printframe(fmt, err, from);
+    from = from->prev;
+  }
+}
+
+static void kllib_tb_printframe(KlFmtConfig* fmt, FILE* err, KlCallInfo* callinfo) {
+  if (!(callinfo->status & KLSTATE_CI_STATUS_KCLO)) {
+    kllib_tb_printCframe(fmt, err);
+    return;
+  }
+  KlKFunction* kfunc = klcast(KlKClosure*, callinfo->callable.clo)->kfunc;
+  KlKFuncFilePosition* posinfo = klkfunc_posinfo(kfunc);
+  KlString* srcfile = klkfunc_srcfile(kfunc);
+  if (!posinfo && !srcfile) {
+    kllib_tb_printKframe_noany(fmt, err);
+    return;
+  }
+  if (!posinfo) {
+    kllib_tb_printKframe_noposinfo(fmt, err, klstring_content(srcfile));
+    return;
+  }
+  if (!srcfile) {
+    KlKFuncFilePosition pos = posinfo[(callinfo->savedpc - klkfunc_entrypoint(kfunc)) - 1];
+    kllib_tb_printKframe_nosrcfile(fmt, err, pos.begin, pos.end);
+    return;
+  }
+  FILE* input = fopen(klstring_content(srcfile), "r");
+  if (kl_unlikely(!input)) {
+    KlKFuncFilePosition pos = posinfo[(callinfo->savedpc - klkfunc_entrypoint(kfunc)) - 1];
+    kllib_tb_printKframe_openfilefailure(fmt, err, klstring_content(srcfile), pos.begin, pos.end);
+    return;
+  }
+  KlKFuncFilePosition pos = posinfo[(callinfo->savedpc - klkfunc_entrypoint(kfunc)) - 1];
+  kllib_tb_printKframe(fmt, err, input, klstring_content(srcfile), pos.begin, pos.end);
+  fclose(input);
+  return;
+}
+
+static void kllib_tb_printCframe(KlFmtConfig* fmt, FILE* err) {
+  fprintf(err, "%s[C] %s:%4s:%4s:\n", fmt->promptmsg, "??", "??", "??");
+  fprintf(err, "%s\n", fmt->promptnorm);
+}
+
+static void kllib_tb_printKframe_noany(KlFmtConfig* fmt, FILE* err) {
+  fprintf(err, "%s[K] no backtrace infomation\n", fmt->promptmsg);
+  fprintf(err, "%s\n", fmt->promptnorm);
+}
+
+static void kllib_tb_printKframe_noposinfo(KlFmtConfig* fmt, FILE* err, const char* inputname) {
+  fprintf(err, "%s[K] %s: offset = (begin = %s, end = %s)\n", fmt->promptmsg, inputname, "unknown", "unknown");
+  fprintf(err, "%s\n", fmt->promptnorm);
+}
+
+static void kllib_tb_printKframe_nosrcfile(KlFmtConfig* fmt, FILE* err, long begin, long end) {
+  fprintf(err, "%s[K] %s: offset = (begin = %ld, end = %ld)\n", fmt->promptmsg, "unknown", begin, end);
+  fprintf(err, "%s\n", fmt->promptnorm);
+}
+
+static void kllib_tb_printKframe_openfilefailure(KlFmtConfig* fmt, FILE* err, const char* inputname, long begin, long end) {
+  fprintf(err, "%s[K] %s: offset = (begin = %ld, end = %ld)\n", fmt->promptmsg,inputname, begin, end);
+  fprintf(err, "%sfailed to open source file.\n", fmt->promptmsg);
+}
+
+static void kllib_tb_printKframe(KlFmtConfig* fmt, FILE* err, FILE* input, const char* inputname, long begin, long end) {
+  long orioffset = ftell(input);
+  unsigned line = kllib_tb_helper_locateline(input, begin);
+  long linebegin = ftell(input);
+
+  unsigned col = begin - linebegin + 1;
+  fprintf(err, "%s[K] %s:%4u:%4u:\n", fmt->promptmsg, inputname, line, col);
+
+  unsigned nputline = 0;
+  while (nputline++ < fmt->maxtextline) {
+    if (!kllib_tb_helper_showline_withcurl(fmt, err, input, begin, end)) {
+      break;
+    }
+  }
+  if (nputline > fmt->maxtextline)
+    fprintf(err, "%stoo many lines...\n", fmt->promptnorm);
+  fseek(input, orioffset, SEEK_SET);
+  fprintf(err, "%s\n", fmt->promptnorm);
+}
+
+#define kl_isnl(ch)       ((ch) == '\n' || (ch) == '\r')
+
+static unsigned kllib_tb_helper_locateline(FILE* input, long offset) {
+  fseek(input, 0, SEEK_SET);
+  unsigned currline = 1;
+  long lineoff = 0;
+  while (lineoff < offset) {
+    int ch = fgetc(input);
+    if (ch == EOF) break;
+    if (kl_isnl(ch)) {
+      ++currline;
+      lineoff = ftell(input);
+      if (lineoff < 0) {
+        lineoff = 0;
+        return currline;
+      }
+    }
+  }
+  fseek(input, lineoff, SEEK_SET);
+  return currline;
+}
+
+static bool kllib_tb_helper_showline_withcurl(KlFmtConfig* fmt, FILE* err, FILE* input, long begin, long end) {
+  long curroffset = ftell(input);
+  if (curroffset >= end) return false;
+  fprintf(err, "%s", fmt->prompttext);
+  int ch = fgetc(input);
+  while (!kl_isnl(ch) && ch != EOF) {
+    if (ch == '\t') {
+      for (size_t i = 0; i < fmt->tabstop; ++i)
+        fputc(' ', err);
+    } else {
+      fputc(ch, err);
+    }
+    ch = fgetc(input);
+  }
+  fputc('\n', err);
+  fprintf(err, "%s", fmt->prompttext);
+  fseek(input, curroffset, SEEK_SET);
+  ch = fgetc(input);
+  /* leading white space should not be underlined. */
+  while (ch == ' ' || ch == '\t') {
+    if (ch == '\t') {
+      for (size_t i = 0; i < fmt->tabstop; ++i)
+        fputc(' ', err);
+    } else {
+      fputc(' ', err);
+    }
+    ch = fgetc(input);
+    ++curroffset;
+  }
+
+  while (!kl_isnl(ch)) {
+    if (curroffset == begin && curroffset == end) {
+      fputc(fmt->zerocurl, err);
+    } else if (curroffset >= begin && curroffset < end) {
+      if (ch == '\t') {
+        for (size_t i = 0; i < fmt->tabstop; ++i)
+          fputc(fmt->curl, err);
+      } else {
+        fputc(fmt->curl, err);
+      }
+    } else {
+      if (ch == '\t') {
+        for (size_t i = 0; i < fmt->tabstop; ++i)
+          fputc(' ', err);
+      } else {
+        fputc(' ', err);
+      }
+    }
+    if (ch == EOF) break;
+    ch = fgetc(input);
+    ++curroffset;
+  }
+  fputc('\n', err);
+  return true;
+}
