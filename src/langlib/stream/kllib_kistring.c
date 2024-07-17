afd23e6aa57ef2a0d41c8be2a4951dd9bce9d839
Ji Kaiwen <kevin0x00@163.com>
2024-07-17 12:41:14 +0800

    fix: read overflow calling readline() on istream.string object

--- src/langlib/stream/kllib_kistring.c
+++ src/langlib/stream/kllib_kistring.c
@@ -1,5 +1,6 @@
 #include "include/langlib/stream/kllib_kistring.h"
 #include "include/mm/klmm.h"
+#include "include/value/klstring.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -20,7 +21,7 @@ Ki* kistring_create(KlString* str) {
   if (!kistring) return NULL;
   kistring->str = str;
   ki_init((Ki*)kistring, &kistring_vfunc);
-  ki_setbuf((Ki*)kistring, klstring_content(str), klstring_size(str), 0);
+  ki_setbuf((Ki*)kistring, klstring_content(str), klstring_length(str), 0);
   return (Ki*)kistring;
 }
 
@@ -29,16 +30,16 @@ static void kistring_delete(KiString* kistring) {
 }
 
 static KioFileOffset kistring_size(KiString* kistring) {
-  return klstring_size(kistring->str);
+  return klstring_length(kistring->str);
 }
 
 static void kistring_reader(KiString* kistring) {
   size_t readpos = ki_tell((Ki*)kistring);
-  if (readpos >= klstring_size(kistring->str)) {
+  if (readpos >= klstring_length(kistring->str)) {
     ki_setbuf((Ki*)kistring, ki_getbuf((Ki*)kistring), 0, readpos);
     return;
   }
-  ki_setbuf((Ki*)kistring, klstring_content(kistring->str), klstring_size(kistring->str), 0);
+  ki_setbuf((Ki*)kistring, klstring_content(kistring->str), klstring_length(kistring->str), 0);
   ki_setbufcurr((Ki*)kistring, readpos);
 }
 
