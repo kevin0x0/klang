57f8910e016afbabddd190cabd62ed71dff5a668
Ji Kaiwen <kevin0x00@163.com>
2024-07-24 17:26:50 +0800

    fix: add klconfig.h

---
+++ include/lang/klconfig.h
@@ -0,0 +1,26 @@
+#ifndef _KLANG_INCLUDE_LANG_KLCONFIG_H_
+#define _KLANG_INCLUDE_LANG_KLCONFIG_H_
+
+#define KLCONFIG_LIBRARY_RTCPL_LIBNAME_QUOTE          ("runtime_compiler.so")
+#define KLCONFIG_LIBRARY_RTCPL_WRAPPER_LIBNAME_QUOTE  ("rtcpl_wrapper.so")
+#define KLCONFIG_LIBRARY_TRACEBACK_LIBNAME_QUOTE      ("traceback.so")
+#define KLCONFIG_LIBRARY_BASIC_LIBNAME_QUOTE          ("basic.so")
+#define KLCONFIG_LIBRARY_PRINT_LIBNAME_QUOTE          ("print.so")
+#define KLCONFIG_LIBRARY_STREAM_LIBNAME_QUOTE         ("stream.so")
+
+#define KLCONFIG_LIBRARY_RTCPL_ENTRYFUNCNAME          kllib_rtcpl_init
+#define KLCONFIG_LIBRARY_RTCPL_WRAPPER_ENTRYFUNCNAME  kllib_rtcpl_wrapper_init
+#define KLCONFIG_LIBRARY_TRACEBACK_ENTRYFUNCNAME      kllib_traceback_init
+#define KLCONFIG_LIBRARY_BASIC_ENTRYFUNCNAME          kllib_basic_init
+#define KLCONFIG_LIBRARY_PRINT_ENTRYFUNCNAME          kllib_print_init
+#define KLCONFIG_LIBRARY_STREAM_ENTRYFUNCNAME         kllib_stream_init
+
+#define KLCONFIG_LIBRARY_RTCPL_ENTRYFUNCNAME_QUOTE          ("kllib_rtcpl_init")
+#define KLCONFIG_LIBRARY_RTCPL_WRAPPER_ENTRYFUNCNAME_QUOTE  ("kllib_rtcpl_wrapper_init")
+#define KLCONFIG_LIBRARY_TRACEBACK_ENTRYFUNCNAME_QUOTE      ("kllib_traceback_init")
+#define KLCONFIG_LIBRARY_BASIC_ENTRYFUNCNAME_QUOTE          ("kllib_basic_init")
+#define KLCONFIG_LIBRARY_PRINT_ENTRYFUNCNAME_QUOTE          ("kllib_print_init")
+#define KLCONFIG_LIBRARY_STREAM_ENTRYFUNCNAME_QUOTE         ("kllib_stream_init")
+
+#endif
+
